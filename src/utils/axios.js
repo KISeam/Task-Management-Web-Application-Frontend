91fd57e63d0753523cce71ccebba92d0a495a2f6
Md. Seam <seam5559@gmail.com>
2025-06-26 01:01:45 +0600

    url update

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: "https://task-management-web-application-backend.onrender.com" || 'http://localhost:10000/api',
+  baseURL: "https://task-management-web-application-backend.onrender.com/api" || 'http://localhost:10000/api',
   withCredentials: true,
 });
 
